df595ad22ccf652760a17cfb7216feab37119dba
Saikiran <saikiran@saikiranks-macbook-air.local>
2019-05-31 11:20:32 +0530

    Initial Commit

---
+++ Admissions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Admissions
+//
+//  Created by SaikiranK on 31/05/19.
+//  Copyright © 2019 SaikiranK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
